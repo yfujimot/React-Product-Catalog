8db54158b8c2e8336edcec1136154893cc793a28
Yoshi <yoshi@simplepickups-MacBook-Air.local>
2015-05-29 14:51:19 -0700

    base template

---
+++ build/.module-cache/0f7c2bf0047253793a8daeea8bd23882b6fcc77a.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement("h1", {id: "logo"}, React.createElement("a", {href: "index.html"}, "Apple Store")),
+  document.getElementById('header')
+);
---
+++ src/script.js
@@ -0,0 +1,4 @@
+React.render(
+  <h1 id="logo"><a href="index.html">Apple Store</a></h1>,
+  document.getElementById('header')
+);
