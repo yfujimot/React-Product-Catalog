0de246bf76c2e0e1fce63e16929a42ec4724349f
Yoshi <yoshi@simplepickups-MacBook-Air.local>
2015-05-29 16:26:12 -0700

    Little more modular

--- build/.module-cache/aeab1a722fb597e5165e872c49f91791b397e6e3.js
+++ build/.module-cache/aeab1a722fb597e5165e872c49f91791b397e6e3.js
@@ -2,6 +2,6 @@ React.render(
   React.createElement("h1", {id: "logo"}, React.createElement("a", {href: "index.html"}, "Apple Store")),
   document.getElementById('header'), 
   function () {
-  	console.log("Yo this rendered bro ")
+  	console.log("Yo this rendered bro")
   }
 );
--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 React.render(
   <h1 id="logo"><a href="index.html">Apple Store</a></h1>,
-  document.getElementById('header')
+  document.getElementById('header'), 
+  function () {
+  	console.log("Yo this rendered bro ")
+  }
 );
